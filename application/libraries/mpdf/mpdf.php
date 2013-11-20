1330cd8d79fbcf556622cb1ce6bc25b5c929c65e
mannysoft <mannysoft@gmail.com>
2013-11-20 12:55:49 +0800

    Upgrade libs

--- application/libraries/mpdf/mpdf.php
+++ application/libraries/mpdf/mpdf.php
@@ -825,6 +825,7 @@ var $innerblocktags;
 
 //function mPDF($mode='',$format='A4',$default_font_size=0,$default_font='',$mgl=15,$mgr=15,$mgt=16,$mgb=16,$mgh=9,$mgf=9, $orientation='P') {
 function mPDF($params = array()) {
+	
 
 //Added 6.22.2011
 	$mode 				= '';
